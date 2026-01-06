df6ed42c0b08fb0953eabbce5879b9c185703328
Demis Bellot <demis.bellot@gmail.com>
2026-01-07 02:04:34 +0800

    add v3 docs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,11 @@
+import type { MDXComponents } from 'mdx/types';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { Steps, Step } from 'fumadocs-ui/components/steps';
 import { CopyBlock } from '@/app/(home)/copy-block';
-import type { MDXComponents } from 'mdx/types';
+import { Info } from '@/components/info';
+import { Tip } from '@/components/tip';
+import { YouTube } from '@/components/youtube';
+import { ScreenshotsGallery } from '@/components/screenshots-gallery';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -9,6 +13,10 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Steps,
     Step,
     ShellCommand: CopyBlock,
+    Info,
+    Tip,
+    YouTube,
+    ScreenshotsGallery,
     ...components,
   };
 }
